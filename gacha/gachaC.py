54f52d56d2f22f549c8e39518ef501b8e4bf8998
beatmil <anutorn@gmail.com>
2020-11-09 23:35:26 +0700

    made another gacha gachaC.py  I should make something else next time I guess  Changes to be committed: 	modified:   gacha/gachaB.py 	new file:   gacha/gachaC.py

--- gacha/gachaC.py
+++ gacha/gachaC.py
@@ -1,16 +1,16 @@
 from random import randint as rd
-# I did this alone!! I cant even see my own comment!
-# use git diff to checkout the different between commits
 inventory = []
-print("Welcome to another %s"%__file__)
-while(True):
+print("welcome to gacha game!")
+while (True):
     ran_num = rd(1,10)
-    if inventory.count(ran_num) > 0:
+    if (inventory.count(ran_num) > 0):
         print("DUP: %s"%ran_num)
     else:
         inventory.append(ran_num)
         inventory.sort()
         print(inventory)
+
     if len(inventory) >= 10:
         break
-print("you win!")
+
+print("You win!")
