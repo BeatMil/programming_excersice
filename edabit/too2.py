43fd45c027ff2c764d9499c968b82b50363523d2
BeatMil <anutorn@gmail.com>
2020-12-16 10:36:13 +0700

    print same text 3 times

--- edabit/too2.py
+++ edabit/too2.py
@@ -1,7 +1,4 @@
-def get_number_of_apples(n, p):
-    percent = int( p[:-1] )
-    result = int(n * ((100 - percent)/100))
-    if result < 1:
-        return "The childen did not get any apples"
-    else:
-        return result
+def front3(text):
+    repeat_text = text[0:3] 
+    return  repeat_text*3
+print(front3("beat"))
