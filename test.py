162190bc04331f2b6b607ae0d5cdfaa2a73f4bae
beatmil <anutorn@gmail.com>
2020-12-30 00:58:23 +0700

    gacha game gets so short now :)

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
+# This gacha game gets a lot shorter than the first time lol
+# I'm getting better at this!
+# Me so gud >///<
 import random as rd
-print(rd.randint(1,10))
+inventory = []
+while (len(inventory) < 10):
+    rand_num = rd.randint(1,10)
+    if rand_num not in inventory:
+        inventory.append(rand_num)
+inventory.sort()
+print(inventory)
